03fb1be5ae53768eafaa4b24aa5a8152e011a55d
GevaX <gevax1010@gmail.com>
2024-07-22 19:38:18 +0300

    Showing files uploaded and implemented a function to format the size of uploaded files

--- src/components/dropzone.js
+++ src/components/dropzone.js
@@ -4,14 +4,14 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { FaCloudUploadAlt } from "react-icons/fa";
 import { LuFileSymlink } from "react-icons/lu";
+import bytesToSize from "@/src/utils/bytesToSize"
 
 export default function Dropzone({ className }) {
     const [isDraggingOver, setIsDraggingOver] = useState(false);
-    const [filesDropped, setFilesDropped] = useState(false);
+    const [filesDropped, setFilesDropped] = useState([]);
 
     const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles);
-        setFilesDropped(true);
+        setFilesDropped(acceptedFiles);
     }, []);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -42,25 +42,31 @@ export default function Dropzone({ className }) {
     }, []);
 
     return (
-        <div {...getRootProps({ className })} className={`${className} ${isDragActive || isDraggingOver ? 'dragging' : ''}`}>
-            <input {...getInputProps()} />
-            {
-                isDragActive || isDraggingOver ? (
-                    <>
-                        <LuFileSymlink className="size-64" />
-                        <span>Drop the files here ...</span>
-                    </>
-                ) : filesDropped ? (
-                    <>
-                        <span>Files dropped successfully!</span>
-                    </>
-                ) : (
-                    <>
-                        <FaCloudUploadAlt className="size-64" />
-                        <span>Drag & drop some files here, or click to select files</span>
-                    </>
-                )
-            }
-        </div>
+        <>
+            {filesDropped.length > 0 ? (
+                <div>
+                    {filesDropped.map(file => (
+                        <div key={file.path} className="relative flex flex-wrap items-center justify-between w-full px-4 py-4 space-y-2 border cursor-pointer lg:py-0 rounded-xl h-fit lg:h-20 lg:px-10 lg:flex-nowrap">
+                            <strong>{file.path}</strong> - {bytesToSize(file.size)}
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div {...getRootProps({ className })} className={`${className} ${isDragActive || isDraggingOver ? 'dragging' : ''}`}>
+                    <input {...getInputProps()} />
+                    {isDragActive || isDraggingOver ? (
+                        <>
+                            <LuFileSymlink className="size-64" />
+                            <span>Drop the files here ...</span>
+                        </>
+                    ) : (
+                        <>
+                            <FaCloudUploadAlt className="size-64" />
+                            <span>Drag & drop some files here, or click to select files</span>
+                        </>
+                    )}
+                </div>
+            )}
+        </>
     );
 }
